1c6a62e5d91d9a4949ce99e191d1e29456dabb10
dcendry1711 <daniel.cendry@gmail.com>
2025-08-31 17:26:32 +0200

    game status section

--- asmb/src/App.jsx
+++ asmb/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <main>
       <Header />
+
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+
     </main> 
   )
 }
@@ -16,8 +22,8 @@ Przegląd projektu:
 
 A. Layout
   1. utworzenie nagłowka zgodnego z projektem na FIGMA (ZROBIONE)
-  2. Utworzenie sekcji ze statusem gry
-  3. Utworzenie sekcji z językami programowania
+  2. Utworzenie sekcji ze statusem gry (na ten moment dane dot. statusu gry będą wprowadzone na sztywno) (ZROBIONE)
+  3. Utworzenie sekcji z językami programowania 
   4. Utowrzenie sekcji z hasłem do odgadnięcia
   5. Utworzenie klawiatury do podawania liter
   6. Utworzenie przycisku "New Game" umożliwiającego rozpoczęcie gry
