17a3979ea8a0c672db3a2a0b99a52f4108a9e6da
dcendry1711 <daniel.cendry@gmail.com>
2025-08-31 17:19:55 +0200

    create header

---
+++ asmb/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <main>
+      <Header />
+    </main> 
+  )
+}
+
+export default App
+
+/*
+Przegląd projektu:
+
+A. Layout
+  1. utworzenie nagłowka zgodnego z projektem na FIGMA (ZROBIONE)
+  2. Utworzenie sekcji ze statusem gry
+  3. Utworzenie sekcji z językami programowania
+  4. Utowrzenie sekcji z hasłem do odgadnięcia
+  5. Utworzenie klawiatury do podawania liter
+  6. Utworzenie przycisku "New Game" umożliwiającego rozpoczęcie gry
+
+B. Działanie
+  1.do ustalenia
+*/
